6b2f26a71969916d7d0b1bbed9b06ddc2c31b8ec
devmdeniz <mehmetdeniz1397@gmail.com>
2020-12-31 16:47:03 +0300

    The Theme is Divided Into Pieces Inside Theme Folder

--- index.php
+++ index.php
@@ -1,22 +1,7 @@
 <html>
-
-<head>
-    <title>Hello World</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
-</head>
+     <?php include("theme/headtags.php");?>
 
 <body>
-    <div class="header">
-        <a href="#default" class="logo">DevMDeniz</a>
-        <div class="header-right">
-            <a class="active" href="#home">Home</a>
-            <a href="#contact">Contact</a>
-            <a href="#about">About</a>
-        </div>
-    </div>
+    <?php include("theme/header.php");?>
 </body>
 </html>
---
+++ theme/headtags.php
@@ -0,0 +1,9 @@
+
+<head>
+    <title>Hello World</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+</head>
