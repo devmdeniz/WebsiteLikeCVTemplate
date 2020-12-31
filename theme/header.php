6982ba7f744836140384ea0452eade177bb3de7e
devmdeniz <mehmetdeniz1397@gmail.com>
2020-12-31 17:28:27 +0300

    Login and Sign Up buttons added and main.js file created

--- theme/header.php
+++ theme/header.php
@@ -1,8 +1,12 @@
 <div class="header">
         <a href="#default" class="logo">DevMDeniz</a>
-        <div class="header-right">
-            <a class="active" href="#home">Home</a>
+        <div class="header-left">
+            <a class="active" href="#">Home</a>
             <a href="#contact">Contact</a>
             <a href="#about">About</a>
         </div>
+        <div class="header-right">
+        <a class="active login" href="#">Log In</a>
+        <a class="active signup" href="#">Sign Up</a>    
+    </div>
     </div>
--- theme/headtags.php
+++ theme/headtags.php
@@ -6,4 +6,5 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+    <script src="main.js"></script>
 </head>
