e611e7886c87c5f2531838bfe306ce18b26e6892
devmdeniz <mehmetdeniz1397@gmail.com>
2021-01-01 00:41:59 +0300

    Added "My Skills" Paragraph

--- index.php
+++ index.php
@@ -1,22 +1,35 @@
 <html>
-     <?php include("theme/headtags.php");?>
+<?php include("theme/headtags.php");?>
 
 <body>
     <?php include("theme/header.php");?>
 
-    <div class ="person">
-    <div class="card">
-  <img src="images/darklogo.png" alt="John" style="width:100%">
-  <h1>DevMDeniz</h1>
-  <p class="title">Ceo Of F-Tech Studios </p>
-  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat, dolorum!</p>
-  <a href="#"><i class="fa fa-instagram socialmediaperson"></i></a>
-  <a href="#"><i class="fa fa-twitter socialmediaperson"></i></a>
-  <a href="#"><i class="fa fa-linkedin socialmediaperson"></i></a>
-  <a href="#"><i class="fa fa-facebook socialmediaperson"></i></a>
-  <br>
-  <p><button class="buttonperson">Contact</button></p>
-</div>
+    <div class="person">
+        <div class="card">
+            <img src="images/darklogo.png" alt="John" style="width:99%">
+            <h1>DevMDeniz</h1>
+            <p class="title">Ceo Of F-Tech Studios </p>
+            <p>If you believe yourself, you can do everything, Just START NOW!</p>
+            <a href="https://instagram.com/dev.mdeniz"><i class="fa fa-instagram socialmediaperson"></i></a>
+            <a href="#"><i class="fa fa-github socialmediaperson"></i></a>
+            <a href="#"><i class="fa fa-linkedin socialmediaperson"></i></a>
+            <a href="#"><i class="fa fa-facebook socialmediaperson"></i></a>
+            <br>
+            <p><button class="buttonperson">Contact Me!</button></p>
+        </div>
     </div>
+     <div class= "main">
+     <h1 class = "myskills">My Skills</h1>
+     <p class= "myskillsp">
+        I started software with NodeJS. I coded the Discord bot for 1 year.
+        Then I wondered about the working systems of websites and started HTML,
+        CSS and Javascript. Then I moved away from Web Development and started
+        trying different things. At these times, I learned Java, Kotlin, Python,
+        C# programming languages and decided to switch to FullStack Web Development.
+     </p>
+</div>
+
+
 </body>
+
 </html>
