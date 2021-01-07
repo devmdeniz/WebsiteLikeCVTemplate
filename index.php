73915cb81064365d25f8112b0afcfa69c7dd6e73
devmdeniz <mehmetdeniz1397@gmail.com>
2021-01-07 14:53:02 +0300

    Made some changes for compatibility in style.css

--- index.php
+++ index.php
@@ -12,14 +12,22 @@ require("languages/".$_SESSION["language"].".php");
 <?php include("theme/headtags.php");?>
 
 <body>
+
+<!--
+    
+php echo $language["profilecardname"]    
+php echo $language["job"]
+php echo $language["profilecarddescription"]
+-->
+
     <?php include("theme/header.php");?>
 
     <div class="person">
         <div class="card">
             <img src="images/darklogo.png" alt="John" style="width:99%">
-            <h1><?php echo $language["profilecardname"]?></h1>
-            <p class="title"><?php echo $language["job"]?></p>
-            <p><?php echo $language["profilecarddescription"]?></p>
+            <h1>DevMDeniz</h1>
+            <p class="title">Lorem.</p>
+            <p></p>
             <a href="https://instagram.com/dev.mdeniz"><i class="fa fa-instagram socialmediaperson"></i></a>
             <a href="https://github.com/devmdeniz"><i class="fa fa-github socialmediaperson"></i></a>
             <a href="https://www.linkedin.com/in/deniz-kumcu-081b00187/"><i class="fa fa-linkedin socialmediaperson"></i></a>
