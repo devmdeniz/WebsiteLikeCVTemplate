7712dda0f581a1996467cdbb580bec5a4f670abb
devmdeniz <mehmetdeniz1397@gmail.com>
2021-01-14 10:04:17 +0300

    CSS folder created and exported to files with .css extension

--- theme/headtags.php
+++ theme/headtags.php
@@ -1,4 +1,5 @@
-
+<?php
+?>
 <head>
     <title>Hello World</title>
     <link rel="stylesheet" href="style.css">
@@ -7,5 +8,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
-    <script src="main.js"></script>
+    <script src="css/main.js"></script>
+    <script src="css/darkmode.css"></script>
 </head>
