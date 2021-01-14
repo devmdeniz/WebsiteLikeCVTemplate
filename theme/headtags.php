1cab36e67ee9d4a41883684b741e692f881ffff2
devmdeniz <mehmetdeniz1397@gmail.com>
2021-01-14 10:08:29 +0300

    Some bugs fixed

--- theme/headtags.php
+++ theme/headtags.php
@@ -2,12 +2,12 @@
 ?>
 <head>
     <title>Hello World</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="darkmode.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
-    <script src="css/main.js"></script>
-    <script src="css/darkmode.css"></script>
+    <script src="main.js"></script>
 </head>
